78732629ea7754f854e0092d5b5a4b4727812aa0
gzavlanis <gzavlanis@gmail.com>
2023-01-19 18:09:45 +0200

    update

--- task_2.js
+++ task_2.js
@@ -1,5 +1,6 @@
 const data = require("./test.json");
 const fs = require('fs');
+const ld = require('lodash');
 
 // get messages for every element and create an array of them
 const getContents = (item) => {
@@ -9,22 +10,26 @@ const getContents = (item) => {
 const contents = data.map(getContents); // the array of content objects
 
 // task 2
-let foundStms = [];
-let foundTotals = [];
+let stms = [];
+let totals = [];
 
 for (let i = 0; i < contents.length; i++) {
     let start = contents[i].bot.indexOf("/");
     let end = contents[i].bot.lastIndexOf("#");
-    if (JSON.stringify(foundTotals).indexOf(JSON.stringify(contents[i].d.live.total)) < 0) {
-        if (contents[i].bot.substring(start, end).indexOf("bots") > 0){
-            let stm = {stm: contents[i].d.stm, total: contents[i].d.live.total};
-            foundStms.push(stm);
+    if (contents[i].bot.substring(start, end).indexOf("bots") > 0){
+        if (typeof contents[i].d.live.total != 'undefined') {
+            if(ld.isEqual(totals, contents[i].d.live.total) == false) {
+                let total = {h: contents[i].d.live.total.h, g: contents[i].d.live.total.g};
+                totals.push(total);
+                let stm = {stm: contents[i].d.stm, total: contents[i].d.live.total};
+                stms.push(stm);
+            }
         }
     }
 }
 
 // show results
-console.log(foundStms);
+console.log(totals);
 
 // write results in json file
 // let json = JSON.stringify(foundStms);
--- task_3.js
+++ task_3.js
@@ -10,14 +10,18 @@ const contents = data.map(getContents); // the array of content objects
 
 // task 3
 let vlData = [];
+let whistles = [];
 
 for (let i = 0; i  < contents.length; i++) {
     let start = contents[i].bot.indexOf("/");
     let end = contents[i].bot.lastIndexOf("#");
     if (contents[i].bot.substring(start, end).indexOf("vlData") > 0) {
         if (contents[i].d.virtualSay != null) {
-            let bot = {bot: contents[i].bot, whistle: contents[i].d.virtualSay.whistle};
-            vlData.push(bot);
+            if(whistles.indexOf(contents[i].d.virtualSay.whistle) < 0){
+                whistles.push(contents[i].d.virtualSay.whistle);
+                let bot = {bot: contents[i].bot, whistle: contents[i].d.virtualSay.whistle};
+                vlData.push(bot);
+            }
         }
     }
 }
@@ -26,5 +30,5 @@ for (let i = 0; i  < contents.length; i++) {
 console.log(vlData);
 
 // write results in json file
-//let json = JSON.stringify(vlData);
-//fs.writeFile("task3.json", json, (err) => err && console.log(err));
+// let json = JSON.stringify(vlData);
+// fs.writeFile("task3.json", json, (err) => err && console.log(err));
